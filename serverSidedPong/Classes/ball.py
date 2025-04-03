71ea9b8fa5aa31e0f44604203b42dfa148fc010d
RafaelMichelB <140518426+RafaelMichelB@users.noreply.github.com>
2025-04-03 19:16:07 +0200

    Update ball.py : Adding fonctions to run simulation indefinitely, with a stop. Still need to establish connexion with AI and Clients

--- serverSidedPong/Classes/ball.py
+++ serverSidedPong/Classes/ball.py
@@ -38,7 +38,13 @@ def calcIntersections(A1 : Point, A2 : Point, B1 : Point, B2 : Point):
 
 	return None, None
 
+def	normalizeNormalVector(vector:Vector) :
+	norm = math.sqrt(vector.x**2 + vector.y**2)
+	return Vector(round(vector.x / norm, 2), round(vector.y / norm, 2))
 
+def calcDotProduct(vec1:Vector, vec2:Vector) :
+	return vec1.x * vec2.x + vec1.y * vec2.y
+	
 class	BallData() :
 	def __init__(self, position:Point=Point(0, 0), speed:Vector=Vector(1, 0)) : # Are 0,0 the center of the game ? 
 		self.pos:Point = position
@@ -49,23 +55,28 @@ class	BallData() :
 
 	def getData(self) -> dict :
 		return ({"position" : [self.pos], "speed" : [self.spd]})
-
+	def calculateReflexionVector(self, wallHit:Vector) -> Vector :
+		normalVector = normalizeNormalVector(wallHit)
+		return Vector(self.spd.x - 2 * calcDotProduct(self.spd, normalVector) * normalVector.x, self.spd.y - 2 * calcDotProduct(self.spd, normalVector) * normalVector.y) le 
+		
 
 class	Movement() :
-	def __init__(self, ballInfo : BallData, racketInfo : list[RacketData], map:Map=Map(), timestamp:float = time.time()) :
+	def __init__(self, ballInfo : BallData, map:Map=Map(), timestamp:float = time.time()) :
+		self.running : bool = True
 		self.ball : BallData = ballInfo
-		self.racket1 : RacketData = racketInfo[0]
-		self.racket2 : RacketData = racketInfo[1]
+		self.racket1 : RacketData | None = None
+		self.racket2 : RacketData | None = None
 		self.ts : float = timestamp
 		self.map : Map = myMap
 
-	def calculateLinearMovement(self, vectorMovement : Vector, time : float) -> Point :
+	async def calculateLinearMovement(self, vectorMovement : Vector, time : float) -> Point :
 		return (Point(self.ball.pos.x + vectorMovement.x * time, self.ball.pos.y + vectorMovement.y * time))
 
-	def isWallIntersection(self, pointTrajectory: Point, timeLeft : float) -> float:
+	async def isWallIntersection(self, pointTrajectory: Point, timeLeft : float) -> float:
 		minTimeWallHit : float = 2.0
 		intersectionPoint = Point(100000, 100000)
 		allWals = self.map + self.racket1.positions + self.racket2.positions
+		wallHit:list | None = None
 		for wall in allWalls:
 			valueIntersections = calcIntersections(wall[0], wall[1], self.ball.pos, pointTrajectory)
 			if valueIntersections[1] != None and valueIntersections[1] < timeLeft and valueIntersections[1] < minTimeWallHit:
@@ -74,20 +85,33 @@ class	Movement() :
 				wallHit = wall
 		if (minTimeWallHit != 2.0) :
 			self.ball.position = intersectionPoint
-			self.ball.spd = self.ball.calculateReflexionVector()
+			self.ball.spd = self.ball.calculateReflexionVector(Vector(wallHit[0], wallHit[1]))
 			return (timeLeft - minTimeWallHit)
 		else :
 			self.ball.pos = pointTrajectory
 			return (0.0)
 		
-	def getWallsHit(self) -> None :
-		timeLeft : float = 1.0
+	async def getWallsHit(self) -> None :
+		timeLeft : float = 0.005
 		while timeLeft > 0.0:
 			linearMovementPoint:Point = self.calculateLinearMovement(self.ball.spd, timeLeft)
 			timeLeft = self.isWallIntersection(linearMovementPoint, timeLeft)
 	
-	def toDictionnary(self) -> dict :
+	async def toDictionnary(self) -> dict :
 		return ({"player1" : self.racket1.getData(), "player2" : self.racket2.getData(), "ball" : self.ball.getData()})
+	async def doSimulation(self) :
+		iteration = 0
+		while running:
+			self.racket1 = dictInfo.get("racket1", Racket())
+			self.racket2 = dictInfo.get("racket2", Racket())
+			self.getWallsHit()
+			iteration += 1
+			if iteration % 10 == 0: #50 ms
+				pass #need to establish how to send data to client
+			if iteration % 200 == 0: # 1000ms , 1 second
+				pass #need to establish how to send data to AI
+	def stopSimultation(self) :
+		self.running = False
 
 	
 
